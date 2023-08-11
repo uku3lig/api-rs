e689cd348fce0cad1603a6c0e1d74ee2c1209d1d
uku <uku3lig@gmail.com>
2023-08-11 19:30:30 +0200

    add initial axum codebase

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::Router;
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new();
+
+    let socket_addr = SocketAddr::from_str("0.0.0.0:3000")?;
+    tracing::info!("listening on {}", socket_addr);
+
+    Ok(axum::Server::bind(&socket_addr)
+        .serve(app.into_make_service())
+        .await?)
 }
