18e16c1178ad3da22d83a52e3482848457a9ed54
uku <hi@uku.moe>
2024-07-20 12:52:52 +0200

    feat: add prometheus metrics

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 
 mod discord;
 mod downloads;
+mod metrics;
 mod tiers;
 mod util;
 
 use crate::util::AppError;
-use anyhow::Result;
 use axum::routing::get;
-use axum::Router;
+use axum::{middleware, Router};
 use once_cell::sync::Lazy;
 use reqwest::header::{HeaderMap, USER_AGENT};
 use reqwest::{Client, StatusCode};
@@ -28,35 +28,41 @@ static CLIENT: Lazy<Client> = Lazy::new(|| {
 });
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     if let Err(e) = dotenvy::dotenv() {
         eprintln!("Failed to load .env file: {e}");
     }
 
     tracing_subscriber::fmt::init();
 
+    tokio::try_join!(start_main_app(), metrics::start_metrics_app())?;
+
+    tracing::info!("shutting down!");
+
+    Ok(())
+}
+
+async fn start_main_app() -> anyhow::Result<()> {
     let app = Router::new()
         .merge(downloads::router())
         .merge(tiers::router())
         .route("/generate_invite", get(discord::generate_invite))
         .fallback(|| async { (StatusCode::NOT_FOUND, "Not Found") })
-        .layer(TraceLayer::new_for_http().on_request(|_: &_, _: &_| {}));
+        .layer(TraceLayer::new_for_http().on_request(|_: &_, _: &_| {}))
+        .layer(middleware::from_fn(metrics::track));
 
     discord::init_bot().await?;
 
     let socket_addr = env::var("SOCKET_ADDR").unwrap_or("0.0.0.0:5000".into());
     let listener = tokio::net::TcpListener::bind(socket_addr).await?;
-    tracing::info!("listening on {}", listener.local_addr()?);
+    tracing::info!("main app listening on {}", listener.local_addr()?);
 
     axum::serve(listener, app)
         .with_graceful_shutdown(async {
             signal(SignalKind::terminate()).unwrap().recv().await;
         })
-        .await?;
-
-    tracing::info!("shutting down!");
-
-    Ok(())
+        .await
+        .map_err(anyhow::Error::from)
 }
 
 type RouteResponse<T> = Result<T, AppError>;
---
+++ src/metrics.rs
@@ -0,0 +1,56 @@
+use std::{env, time::Instant};
+
+use axum::{extract::Request, middleware::Next, response::IntoResponse, routing::get, Router};
+use metrics_exporter_prometheus::{Matcher, PrometheusBuilder};
+use tokio::signal::unix::{signal, SignalKind};
+
+const TOTAL_REQS_KEY: &str = "api_rs_total_requests";
+const REQ_DURATION_KEY: &str = "api_rs_request_duration";
+
+const EXPONENTIAL_SECONDS: &[f64] = &[
+    0.001, 0.002, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+];
+
+pub async fn start_metrics_app() -> anyhow::Result<()> {
+    let handle = PrometheusBuilder::new()
+        .set_buckets_for_metric(
+            Matcher::Full(REQ_DURATION_KEY.to_string()),
+            EXPONENTIAL_SECONDS,
+        )?
+        .install_recorder()?;
+
+    let app = Router::new().route("/metrics", get(move || std::future::ready(handle.render())));
+
+    let socket_addr = env::var("METRICS_SOCKET_ADDR").unwrap_or("127.0.0.1:5001".into());
+    let listener = tokio::net::TcpListener::bind(socket_addr).await?;
+    tracing::info!("metrics listening on {}", listener.local_addr()?);
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(async {
+            signal(SignalKind::terminate()).unwrap().recv().await;
+        })
+        .await
+        .map_err(anyhow::Error::from)
+}
+
+pub async fn track(request: Request, next: Next) -> impl IntoResponse {
+    let start = Instant::now();
+    let method = request.method().clone();
+    let path = request.uri().path().to_owned();
+
+    let response = next.run(request).await;
+
+    let status = response.status().as_u16().to_string();
+    let delta_time = start.elapsed().as_secs_f64();
+
+    let labels = [
+        ("method", method.to_string()),
+        ("path", path),
+        ("status", status),
+    ];
+
+    metrics::counter!(TOTAL_REQS_KEY, &labels).increment(1);
+    metrics::histogram!(REQ_DURATION_KEY, &labels).record(delta_time);
+
+    response
+}
